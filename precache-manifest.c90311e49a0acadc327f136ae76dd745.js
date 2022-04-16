76d0605d921bfb0ed91e440996a9c37e6a05a042
Frael <frael1984@gmail.com>
2022-04-15 23:25:02 -0500

    Updates

---
+++ precache-manifest.c90311e49a0acadc327f136ae76dd745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace2cf3969e83a046869b6425f185ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd12ff8f9d2b897f5b0",
+    "url": "/static/css/2.52579c41.chunk.css"
+  },
+  {
+    "revision": "cdd12ff8f9d2b897f5b0",
+    "url": "/static/js/2.637b9069.chunk.js"
+  },
+  {
+    "revision": "1fb643b8b22fd8b0a63a895f1f55ca64",
+    "url": "/static/js/2.637b9069.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a5df850e42aee179476",
+    "url": "/static/js/main.8b9eb524.chunk.js"
+  },
+  {
+    "revision": "d42caf866377dbafd32b",
+    "url": "/static/js/runtime-main.6ec39be9.js"
+  }
+]);
